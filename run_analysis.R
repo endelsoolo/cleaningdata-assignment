3cd5f2505d54043d3339ce31ff8be37a7bf72a7c
Endel Soolo <endel.soolo@gmail.com>
2016-11-07 04:43:53 +0200

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,118 @@
+# Download the data files
+
+zip <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",zip)
+
+# Read the data from the files
+
+X_train <- read.table(unz(zip, "UCI HAR Dataset/train/X_train.txt"))
+Y_train <- read.table(unz(zip, "UCI HAR Dataset/train/y_train.txt"))
+subject_train <- read.table(unz(zip, "UCI HAR Dataset/train/subject_train.txt"))
+
+body_acc_x_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt"))
+body_acc_y_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt"))
+body_acc_z_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt"))
+
+body_gyro_x_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt"))
+body_gyro_y_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt"))
+body_gyro_z_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt"))
+
+total_acc_x_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt"))
+total_acc_y_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt"))
+total_acc_z_train <- read.table(unz(zip, "UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt"))
+
+X_test <- read.table(unz(zip, "UCI HAR Dataset/test/X_test.txt"))
+Y_test <- read.table(unz(zip, "UCI HAR Dataset/test/y_test.txt"))
+subject_test <- read.table(unz(zip, "UCI HAR Dataset/test/subject_test.txt"))
+
+body_acc_x_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt"))
+body_acc_y_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt"))
+body_acc_z_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt"))
+
+body_gyro_x_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt"))
+body_gyro_y_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt"))
+body_gyro_z_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt"))
+
+total_acc_x_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt"))
+total_acc_y_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt"))
+total_acc_z_test <- read.table(unz(zip, "UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt"))
+
+activity_labels <- read.table(unz(zip, "UCI HAR Dataset/activity_labels.txt"))
+features <- read.table(unz(zip, "UCI HAR Dataset/features.txt"))
+
+# Remove the downloaded file when it is no longer needed
+
+unlink(zip)
+remove(zip)
+
+# Merge the training and testing datasets
+
+X <- rbind(X_train, X_test)
+Y <- rbind(Y_train, Y_test)
+subject <- rbind(subject_train, subject_test)
+
+body_acc_x <- rbind(body_acc_x_train, body_acc_x_test)
+body_acc_y <- rbind(body_acc_y_train, body_acc_y_test)
+body_acc_z <- rbind(body_acc_z_train, body_acc_z_test)
+
+body_gyro_x <- rbind(body_gyro_x_train, body_gyro_x_test)
+body_gyro_y <- rbind(body_gyro_y_train, body_gyro_y_test)
+body_gyro_z <- rbind(body_gyro_z_train, body_gyro_z_test)
+
+total_acc_x <- rbind(total_acc_x_train, total_acc_x_test)
+total_acc_y <- rbind(total_acc_y_train, total_acc_y_test)
+total_acc_z <- rbind(total_acc_z_train, total_acc_z_test)
+
+# Clean memory of duplicate datasets
+
+remove(body_acc_x_train, body_gyro_x_train, total_acc_x_train)
+remove(body_acc_y_train, body_gyro_y_train, total_acc_y_train)
+remove(body_acc_z_train, body_gyro_z_train, total_acc_z_train)
+remove(body_acc_x_test, body_gyro_x_test, total_acc_x_test)
+remove(body_acc_y_test, body_gyro_y_test, total_acc_y_test)
+remove(body_acc_z_test, body_gyro_z_test, total_acc_z_test)
+remove(X_train, X_test, Y_train, Y_test, subject_train, subject_test)
+
+# Convert to data frame
+data <- as.data.frame(X)
+names(data)<-features[[2]]
+remove(X, features)
+
+# Find number of records
+len <- length(data[,1])
+
+# Add inertial data into the same data frame with rest of the data
+data$body_acc_x<-lapply(1:len, function(i) as.vector(sapply(body_acc_x, '[[', i)))
+data$body_acc_y<-lapply(1:len, function(i) as.vector(sapply(body_acc_y, '[[', i)))
+data$body_acc_z<-lapply(1:len, function(i) as.vector(sapply(body_acc_z, '[[', i)))
+
+data$body_gyro_x<-lapply(1:len, function(i) as.vector(sapply(body_gyro_x, '[[', i)))
+data$body_gyro_y<-lapply(1:len, function(i) as.vector(sapply(body_gyro_y, '[[', i)))
+data$body_gyro_z<-lapply(1:len, function(i) as.vector(sapply(body_gyro_z, '[[', i)))
+
+data$total_acc_x<-lapply(1:len, function(i) as.vector(sapply(total_acc_x, '[[', i)))
+data$total_acc_y<-lapply(1:len, function(i) as.vector(sapply(total_acc_y, '[[', i)))
+data$total_acc_z<-lapply(1:len, function(i) as.vector(sapply(total_acc_z, '[[', i)))
+
+remove(body_acc_x, body_gyro_x, total_acc_x)
+remove(body_acc_y, body_gyro_y, total_acc_y)
+remove(body_acc_z, body_gyro_z, total_acc_z)
+remove(len)
+
+# Add subject identifiers to the dataset
+
+data$subject<-subject
+remove(subject)
+
+# Add descriptive activity names to the data set
+
+data$activity<-factor(sapply(Y, function(i) activity_labels[[2]][i]))
+remove(Y, activity_labels)
+
+# Extract only the measurements on the mean and standard deviation for each measurement.
+
+extracted<-data[,grep("mean|std|subject|activity",names(data))]
+
+#library(data.table)
+#DT[,mean(),by=c(subject,activity)]
+#xtabs(Freq ~ Gender + Admit, data=DF)
